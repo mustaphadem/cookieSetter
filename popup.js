0aa79475a611dfe0276970a4ba8732dc6ec1f954
Mustafa Taha Demirci <mdemirci@gofundme.com>
2022-10-04 00:36:11 +0300

    Uses current url from tab, reloads on apply.

--- popup.js
+++ popup.js
@@ -5,22 +5,23 @@ let cookieValue = document.getElementById("cookie-value");
 
 applyChanges.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  setCookies(cookieName.value, cookieValue.value);
+  setCookies(cookieName.value, cookieValue.value, tab);
 });
 
-function setCookies(name, val) {
-  chrome.cookies.get({ name: name, url: "https://www.gofundme.com" }, (cookie) => {
+function setCookies(name, val, tab) {
+  chrome.cookies.get({ name: name, url: tab.url }, (cookie) => {
 
     if (cookie != null) {
-      chrome.cookies.remove({ name: name, url: "https://www.gofundme.com" }, (removedCookie) => {
+      chrome.cookies.remove({ name: name, url: tab.url }, (removedCookie) => {
         console.log(`Removed previous cookie (name: ${removedCookie.name})`);
-        chrome.cookies.set({ name: name, url: "https://www.gofundme.com", value: val }, (c) => {
+        chrome.cookies.set({ name: name, url: tab.url, value: val }, (c) => {
           console.log(`Cookie ${c.name} has been set as ${c.value}`);
+          chrome.tabs.reload();
         });
       });
     } else {
       console.log(`${name} is not found.`);
-      chrome.cookies.set({ name: name, url: "https://www.gofundme.com", value: val }, (c) => {
+      chrome.cookies.set({ name: name, url: tab.url, value: val }, (c) => {
         console.log(`New cookie ${c.name} has been set as ${c.value}`);
       });
     };
