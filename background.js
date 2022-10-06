f6ebe43a43a54aca6bdef25cf870bc303aeb6676
Mustafa Taha Demirci <mdemirci@gofundme.com>
2022-10-06 22:43:01 +0300

    v2-saved cookies

--- background.js
+++ background.js
@@ -1,15 +1,12 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.cookies.get({name:"gdid", url:"https://www.gofundme.com"}, (cookie) => {
+chrome.webNavigation.onCompleted.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    chrome.cookies.get({name:"gdid", url:tab.url}, (cookie) => {
       if(cookie != null){
         console.log(`gdid is set as ${cookie.value}`);
       } else {
         console.log("gdid is not set.");
-      }
-      
+      } 
   });
-  // chrome.storage.sync.set({ color });
-  // console.log('Default background color set to %cgreen', `color: ${color}`);
+  });
 });
 
--- popup.js
+++ popup.js
@@ -1,31 +1,72 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
 let cookieName = document.getElementById("cookie-name");
 let cookieValue = document.getElementById("cookie-value");
+let save = document.getElementById("cookie-save");
+let shouldSave = document.getElementById("cookie-save").checked;
+let savedOptions = document.getElementById("saved-cookies");
+let savedItems = [];
+
+chrome.storage.sync.get("savedCookiePairs", (data) => {
+  if(!(Object.values(data)[0]==undefined)){
+    savedItems = Object.values(data)[0];
+  }
+ });
+
+save.addEventListener("click", () => {
+  shouldSave = save.checked;
+});
+
+savedOptions.addEventListener("change", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let name = savedOptions.options[savedOptions.selectedIndex].dataset.name;
+  let value = savedOptions.options[savedOptions.selectedIndex].dataset.value;
+  setCookies(name, value, tab);
+})
 
 applyChanges.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  setCookies(cookieName.value, cookieValue.value, tab);
+  let name = cookieName.value;
+  let value = cookieValue.value;
+  if(shouldSave && !isEmpty([name, value])){
+    savedItems.push({[name]:value});
+    chrome.storage.sync.set({"savedCookiePairs": savedItems}, () => {
+      console.log(`Cookie name-value has been saved in storage as ${name} : ${value}`);
+    });
+  };
+  setCookies(name, value, tab);
 });
 
+function isEmpty(values){
+  let flag = false;
+  for (let i = 0; i < values.length; i++) {
+      if(values[i].trim().length == 0){
+          flag = true;
+          break;
+        }
+      
+  }
+  return flag;
+}
+
 function setCookies(name, val, tab) {
-  chrome.cookies.get({ name: name, url: tab.url }, (cookie) => {
-
-    if (cookie != null) {
-      chrome.cookies.remove({ name: name, url: tab.url }, (removedCookie) => {
-        console.log(`Removed previous cookie (name: ${removedCookie.name})`);
-        chrome.cookies.set({ name: name, url: tab.url, value: val }, (c) => {
-          console.log(`Cookie ${c.name} has been set as ${c.value}`);
-          chrome.tabs.reload();
-        });
-      });
-    } else {
-      console.log(`${name} is not found.`);
-      chrome.cookies.set({ name: name, url: tab.url, value: val }, (c) => {
-        console.log(`New cookie ${c.name} has been set as ${c.value}`);
-        chrome.tabs.reload();
-      });
-    };
-
+  chrome.cookies.set({ name: name, url: tab.url, value: val }, (c) => {
+    console.log(`New cookie ${c.name} has been set as ${c.value}`);
   });
+  chrome.tabs.reload();
 };
+
+async function constructSavedItems() {
+  await chrome.storage.sync.get("savedCookiePairs", (data) => {
+    if(!(Object.values(data)[0]==undefined)){
+      savedItems = Object.values(data)[0];
+      for (let cookiePair of savedItems){
+        let option = document.createElement("option");
+        option.dataset.name = Object.keys(cookiePair)[0];
+        option.dataset.value = Object.values(cookiePair)[0];
+        option.label = Object.keys(cookiePair)[0] + " - " + Object.values(cookiePair)[0];
+        savedOptions.appendChild(option);
+      }
+    };
+   });
+};
+
+constructSavedItems();
